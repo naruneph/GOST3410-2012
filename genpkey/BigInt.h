cd842f283531c0bd11a28f9bbef1cf5f38401bfd
naruneph <ea.shlyakhtina@gmail.com>
2019-04-02 01:57:34 +0300

    .

--- genpkey/BigInt.h
+++ genpkey/BigInt.h
@@ -34,15 +34,16 @@ class BigInt{
 
 		BigInt operator+(const BigInt &) const;
 		BigInt operator-(const BigInt &) const;
+
+		void inc();
+
 		BigInt operator*(const BigInt &) const;
 		vector<BigInt> operator/(const BigInt &) const;
-//		BigInt operator%(const BigInt &) const;
 
-		BigInt gcd(BigInt, BigInt, BigInt &, BigInt &);
+		BigInt gcd(BigInt &, BigInt &, BigInt &, BigInt &);
 		BigInt inv_mod(BigInt &);
-
-		void shift_r();
 };
+
 
 void BigInt::remove_leading_zeroes(){
 	while(value.size() > 1 && value.back() == 0x00){
@@ -197,125 +198,116 @@ BigInt BigInt::operator-(const BigInt &num) const{
 	return a;
 }
 
+bool IsBitSet(uint8_t num, int bit){ //вспомогательная функция
+	return 1 == ( (num >> bit) & 1);
+}
+
+void BigInt::inc(){
+	uint8_t carry = 0x00;
+	for(int i = 0; i < this->size(); i++){
+		uint8_t tmp = (value[i] & 0x80) >> 7;
+		value[i] = (value[i] << 1) | carry;
+		carry = tmp;
+	}
+	if (carry!= 0x00){
+		value.push_back(carry);
+	}
+}
+
 BigInt BigInt::operator*(const BigInt &num) const{
 	BigInt a(*this), b(num);
 	a.remove_leading_zeroes();
 	b.remove_leading_zeroes();
-
-	BigInt res;
-	for(unsigned int i = 0; i < a.value.size() + b.value.size(); i++){
-		res.value.push_back(0x00);
-	}
- 	res.positive = (a.positive == b.positive);
-
-	for(unsigned int i = 0; i < a.value.size(); i++){
-		uint8_t carry = 0;
-
-		for(unsigned int j = 0; j < b.value.size() || carry != 0x00; j++){
-			uint16_t tmp = res.value[i+j] + a.value[i] *(j < b.value.size() ?  b.value[j] : 0x00) + carry;
-			res.value[i+j] = tmp & 0xff;
-			carry = (tmp >> 8);
+	a.positive = true;
+	b.positive = true;
+	BigInt result;
+	result.positive = (this->positive == num.positive);
+	bool init = true;
+	for(int i = 0; i < b.size(); i++){
+		for(int j = 0; j < 8; j++){
+			if(!init){
+				a.inc();
+			} 
+			if (i == 0 && j == 0){
+				init = false;
+			}
+			uint8_t bb = b.value[i];
+			if(IsBitSet(bb, j)){
+				result += a;
+			}
 		}
 	}
-	res.remove_leading_zeroes();
+	return result;
+}
+
+void set_least_bit(uint8_t & r, uint8_t a, int j){//вспомогательная функция
+	a = (a >> j) & 0x01;
+	uint8_t tmp = r & 0x01;
+	if(a != tmp){
+		r = r ^ 0x01;
+	}
+}
+
+void set_bit_1(uint8_t & q, int j){//вспомогательная функция
+	uint8_t tmp = 0x01 << j;
+	q = q | tmp;
+}
+
+vector<BigInt> BigInt::operator/(const BigInt &num) const{
+	BigInt a(*this), b(num);
+	a.remove_leading_zeroes();
+	b.remove_leading_zeroes();
+	a.positive = true;
+	b.positive = true;
+
+	BigInt zero = vector<uint8_t> {0x00};
+	BigInt one = vector<uint8_t> {0x01};
+
+	if(b == zero){
+		throw (string) "DIVISION_BY_ZERO";
+	} else if(a == b) {
+		if(this->positive == num.positive){
+			return vector<BigInt> {one, zero};
+		} else {
+			return vector<BigInt> {-one, zero};
+		}
+	} else if(a < b){
+		if(positive){
+			return vector<BigInt> {zero, a};
+		} else{
+			return vector<BigInt> {zero, b - a};
+		}
+	}
+
+	BigInt Q(a.size());// частное
+	BigInt R = zero;// остаток
+
+	for(int i = a.size() - 1; i >= 0; i--){
+		for(int j = 7; j >= 0; j--){
+			R.inc();
+			set_least_bit(R.value[0], a.value[i], j); 
+			if(R >= b){
+				R -= b;
+				set_bit_1(Q.value[i], j);
+			}
+		}
+	}
+
+	Q.remove_leading_zeroes();
+	Q.positive = (this->positive == num.positive);
+
+	R.remove_leading_zeroes();
+	if(this->positive != num.positive){
+		R = b - R;
+	}
+
+	vector<BigInt> res = {Q, R};
+
 	return res;
 }
 
 
-// BigInt BigInt::operator/(const BigInt &num) const{// деление на ноль??? a/b
-// 	BigInt a(*this), b(num);
-// 	a.remove_leading_zeroes();
-// 	b.remove_leading_zeroes();
-// 	a.positive = true;
-// 	b.positive = true;
-
-// 	BigInt zero = vector<uint8_t> {0x00};
-// 	BigInt one = vector<uint8_t> {0x01};
-
-// 	BigInt count = zero;
-
-// 	while( a >= b){
-// 		a -= b;
-// 		count += one;
-// 	}
-
-// 	count.positive = (this->positive == num.positive);
-// 	count.remove_leading_zeroes(); // чтобы 0 был положительным
-// 	return count;
-// }
-
-void BigInt::shift_r(){
-	if(this->value.size() == 0){
-		this->value.push_back(0x00);
-		
-		return;
-	}
-	this->value.push_back(this->value[this->value.size() - 1]);
-	for(int i = this->value.size() - 2; i > 0; i--){
-		this->value[i] = this->value[i-1];
-	}
-	this->value[0] = 0x00;
-}
-
-vector<BigInt> BigInt::operator/(const BigInt &num) const{
- 	BigInt a(*this), b(num);
- 	a.remove_leading_zeroes();
- 	b.remove_leading_zeroes();
- 	a.positive = true;
-	b.positive = true;
-
-	//проверить на деление на ноль
-
-	BigInt res(a.value.size());
-	BigInt cur;
-	for(int i = a.value.size() - 1; i >= 0; i--){
-		cur.shift_r();
-		cur.value[0] = a.value[i];
-		// подбираем максимальное число x, такое что b * x <= cur
-		uint16_t x = 0x00, l = 0x00, r = 0x100;
-		while(l <= r){
-			uint8_t mm = (l + r) >> 1;    
-			BigInt m = vector<uint8_t> {mm};
-			BigInt t = b * m;
-			if(t <= cur){
-				x = mm;  
-				l = mm + 0x01;
-			} else {
-				r = mm - 0x01;
-			}
-		}
-		res.value[i] = (uint8_t) x; 
-		cur = cur - b * (BigInt) vector<uint8_t> {(uint8_t) x}; 
-	}// есть подозрение что cur это a % b  и похоже это реально так
-
-	// vector<uint8_t> aa = cur.val();
-	// for(int i=aa.size() -1; i >=0 ; i--){
-	// 	printf("%02x", aa[i]);
-	// }
-
-	// cout<<endl;
-	// cout<< cur.sgn();
-
-	res.positive = (this->positive == num.positive);
-	res.remove_leading_zeroes();
-	//return res;
-	return vector<BigInt> {res,cur};
-}
-
-
-//BigInt BigInt::operator%(const BigInt &num) const{
-	// BigInt a(*this), b(num);
-	// a.remove_leading_zeroes();
-	// b.remove_leading_zeroes();
-
-	// BigInt res = a - (a/b) * b;
-	// if(!res.positive){
-	// 	res += b;
-	// }
-	// return res;
-//}
-
-BigInt BigInt::gcd(BigInt a, BigInt b, BigInt &x, BigInt &y){
+BigInt BigInt::gcd(BigInt &a, BigInt &b, BigInt &x, BigInt &y){
 	BigInt zero = vector<uint8_t> {0x00};
 	BigInt one = vector<uint8_t> {0x01};
 
@@ -328,6 +320,7 @@ BigInt BigInt::gcd(BigInt a, BigInt b, BigInt &x, BigInt &y){
 	vector<BigInt> q = b / a;
 	BigInt d = gcd(q[1], a, x1, y1);
 	x = y1 - (q[0]) * x1;
+
 	y = x1;
 	return d;
 }
@@ -339,7 +332,16 @@ BigInt BigInt::inv_mod(BigInt & num){
 	if (g != one){
 		throw (string) "CANNOT_GET_INVERSE_ELEMENT";
 	}
-//	x = (x % num + num) % num;
-	x = (x / num)[1];
+
+	x = (((x/num)[1] + num)/ num)[1];
 	return x;
 }
+
+
+
+
+
+
+
+
+
